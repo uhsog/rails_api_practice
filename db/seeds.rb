8f6b5dc4f81a565c7fdb37858aa4a577abaa1c5b
uhsog <30695524+uhsog@users.noreply.github.com>
2020-01-12 19:11:18 +0900

    Set for cors & fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Author.find_or_create_by!(name = '') do |author|
+#   author.name = 'Shuhei Goto'
+#   author.birth = Date.new(1993, 6, 1)
+#   author.from = 'Japan, Kumamoto'
+# end
+
+# Author.create!(name: 'Shuhei Goto', birth: Date.new(1993, 6, 1), from: 'Japan, Kumamoto')
+
+require 'faker'
+
+(1..10).map do
+  Author.create!(
+    name: Faker::Name.name,
+    birth: Faker::Date.birthday(min_age: 30, max_age: 65),
+    from: %w[Japan USA Germany Canada UK'].sample)
+end
