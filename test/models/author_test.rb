974beeb59d5a62894ffd5b2bf3d86eadb59a8180
uhsog <30695524+uhsog@users.noreply.github.com>
2020-01-22 23:51:45 +0900

    Faild test

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
   test 'validate name length' do
-    author = Author.new(name: 'a' * 256)
+    author = Author.new(name: 'a' * 255)
     assert_not author.valid?
   end
 end
