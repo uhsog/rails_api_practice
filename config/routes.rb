cb7581b71f7dc4c02917c364d9ed53aff3cb5af3
uhsog <30695524+uhsog@users.noreply.github.com>
2020-02-11 23:18:56 +0900

    Add routing for book

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :ams do
-    resources :authors
+    resources :authors, only: %i[index create update destroy show]
+    resources :books, only: %i[index create update destroy show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
