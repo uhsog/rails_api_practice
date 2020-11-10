faae2df344f5b574b1b9f8185668306625dd55d1
uhsog <30695524+uhsog@users.noreply.github.com>
2020-11-10 09:00:45 +0900

    Fix url for jb_authors

--- test/controllers/jb/authors_controller_test.rb
+++ test/controllers/jb/authors_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Jb::AuthorsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get jb_authors_index_url
+    get jb_authors_url
     assert_response :success
   end
 
